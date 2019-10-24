6b6f46d02ce35ed645a59796548c41c436788bbc
Florian Feldhaus <florian.feldhaus@gmail.com>
2019-10-24 23:51:05 +0100

    added modified docker-entrypoint.sh to run nvidia-xconfig

---
+++ docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+GPU_DEVICE=/usr/local/nvidia/bin/nvidia-xconfig --query-gpu-info | grep PCI | awk -F : '{ print $2":"$3":"$4":"$5 }'
+/usr/local/nvidia/bin/nvidia-xconfig -a --allow-empty-initial-configuration --busid $GPU_DEVICE
+
+su - xpra
+
+# first arg is `-f` or `--some-option`
+if [ "${1:0:1}" = '-' ]; then
+	set -- xpra start "$@"
+fi
+
+exec "$@"
