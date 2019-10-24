d44fa516b963d716a3a2bf9731313aa6f8fab3d0
Florian Feldhaus <florian.feldhaus@gmail.com>
2019-10-25 00:00:15 +0100

    fixed small issue in docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-GPU_DEVICE=/usr/local/nvidia/bin/nvidia-xconfig --query-gpu-info | grep PCI | awk -F : '{ print $2":"$3":"$4":"$5 }'
+GPU_DEVICE=$(/usr/local/nvidia/bin/nvidia-xconfig --query-gpu-info | grep PCI | awk -F : '{ print $2":"$3":"$4":"$5 }')
 /usr/local/nvidia/bin/nvidia-xconfig -a --allow-empty-initial-configuration --busid $GPU_DEVICE
 
 su - xpra
