51b858776144d13b222c128a4b4af55d382a4b26
Florian Feldhaus <florian.feldhaus@gmail.com>
2019-10-29 16:08:05 +0100

    changed nvidia-xconfig

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@
 #set -e
 
 GPU_DEVICE=$(/usr/local/nvidia/bin/nvidia-xconfig --query-gpu-info | grep PCI | awk -F : '{ print $2":"$3":"$4":"$5 }')
-/usr/local/nvidia/bin/nvidia-xconfig -a --busid $GPU_DEVICE --xconfig /etc/xpra/xorg.conf --output-xconfig /etc/xpra/xorg.conf.nvidia
+/usr/local/nvidia/bin/nvidia-xconfig -a --busid $GPU_DEVICE --allow-empty-initial-configuration --use-display-device=None --virtual=1920x1200
 
 exec /usr/bin/xpra start --daemon=no --start-child="$@"
