63184fe9dce42e5635b42997388f2f7828c911eb
Florian Feldhaus <florian.feldhaus@gmail.com>
2019-10-29 08:07:26 +0100

    Changed output path of xorg conf

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 GPU_DEVICE=$(/usr/local/nvidia/bin/nvidia-xconfig --query-gpu-info | grep PCI | awk -F : '{ print $2":"$3":"$4":"$5 }')
-/usr/local/nvidia/bin/nvidia-xconfig -a --allow-empty-initial-configuration --busid $GPU_DEVICE
+/usr/local/nvidia/bin/nvidia-xconfig -a --allow-empty-initial-configuration --busid $GPU_DEVICE --output-xconfig /etc/xpra/xorg.conf
 
 # first arg is `-f` or `--some-option`
 if [ "${1:0:1}" = '-' ]; then
