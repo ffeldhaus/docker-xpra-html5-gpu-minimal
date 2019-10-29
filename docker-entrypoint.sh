2e6a5964d662c89129cfbd5dc3e902305b2ac197
Florian Feldhaus <florian.feldhaus@gmail.com>
2019-10-29 08:45:38 +0100

    Testing xorg configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 GPU_DEVICE=$(/usr/local/nvidia/bin/nvidia-xconfig --query-gpu-info | grep PCI | awk -F : '{ print $2":"$3":"$4":"$5 }')
-/usr/local/nvidia/bin/nvidia-xconfig -a --allow-empty-initial-configuration --busid $GPU_DEVICE --output-xconfig /etc/xpra/xorg.conf
+/usr/local/nvidia/bin/nvidia-xconfig -a --allow-empty-initial-configuration --busid $GPU_DEVICE --xconfig /etc/xpra/xorg.conf --output-xconfig /etc/xpra/xorg.conf
 
 # first arg is `-f` or `--some-option`
 if [ "${1:0:1}" = '-' ]; then
@@ -11,3 +11,5 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 exec "$@"
+
+exec "/bin/bash"
