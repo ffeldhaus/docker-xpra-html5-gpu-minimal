bbe938248cbe45d6006cb4c4728df5916d52435e
Florian Feldhaus <florian.feldhaus@gmail.com>
2020-09-04 22:59:29 +0000

    Several improvements to be able to run xpra with the xpra user instead of root

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ffeldhaus/docker-xpra-html5
 
-LABEL version="0.3"
+LABEL version="0.4"
 LABEL maintainer="florian.feldhaus@gmail.com"
 
 ARG VIRTUALGL_VERSION=2.6.80
@@ -29,9 +29,6 @@ COPY docker-entrypoint.sh /docker-entrypoint.sh
 
 # ensure that all OpenGL applications started via vglrun are using video mode for best display quality in HTML5 client
 RUN echo "\nclass-instance:vglrun=video" >> /usr/share/xpra/content-type/50_class.conf
-
-# using xpra user does not work currently
-#USER xpra
 
 # passing the command does not work currently 
 #CMD ["/opt/VirtualGL/bin/vglrun","-d","/dev/dri/card0","/opt/VirtualGL/bin/glxspheres64"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# configure dbus
+dbus-uuidgen > /var/lib/dbus/machine-id
+mkdir -p /var/run/dbus
+dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
+
 # ensure that xpra user is part of vglusers group which must have been set for /dev/dri/card0
 VGLUSERS_GID=$(ls -ln /dev/dri/card0 | awk '{print $4}')
 groupadd --gid $VGLUSERS_GID vglusers
 usermod --groups $VGLUSERS_GID --append xpra
 
-# start xpra with command specified in dockerfile as CMD or passed as parameter to docker run
-exec /usr/bin/xpra start --daemon=no --start-child="$@"
+# start xpra as xpra user with command specified in dockerfile as CMD or passed as parameter to docker run
+runuser -l xpra -c '/usr/bin/xpra start --daemon=no --start-child="$@"'
