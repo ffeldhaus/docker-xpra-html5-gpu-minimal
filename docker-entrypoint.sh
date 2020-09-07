1ee07259451f2fdadde30f7c270bb0b6ee0ae355
Florian Feldhaus <florian.feldhaus@gmail.com>
2020-09-07 11:05:40 +0000

    fixed quotes to start xpra child command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,4 +11,4 @@ DRI_RENDER_GID=$(ls -ln /dev/dri/render* | head -1 | awk '{print $4}')
 usermod --groups $DRI_CARD_GID,$DRI_RENDER_GID --append xpra
 
 # start xpra as xpra user with command specified in dockerfile as CMD or passed as parameter to docker run
-runuser -l xpra -c '/usr/bin/xpra start --daemon=no --start-child="$@"'
+runuser -l xpra -c "/usr/bin/xpra start --daemon=no --start-child='$@'"
